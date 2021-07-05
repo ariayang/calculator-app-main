dc17b89d69d73605b8aeae6cf19a338d0de1280c
Aria Yang <ariayang@As-MacBook-Pro.local>
2021-07-04 17:24:12 -0700

    theme color

--- index.js
+++ index.js
@@ -193,3 +193,89 @@ $(document).keydown(function (event) {
 
 //TODO: change themes
 // when clicked toggle bar, add class for specific elements, or just change colors in script.
+
+$("#theme1").click(function() {
+    if($("#theme1").is(":checked")){
+        $("body").css("background-color", "hsl(222, 26%, 31%)");
+        $("a").css("color", "#fff");
+        $(".calcText, .theme-nums, .theme-text").css("color", "#fff");
+        $(".switch-toggle").css("background", "hsl(223, 31%, 20%)");
+        $(".switch-toggle label").css("background", "transparent");
+        $(".switch-toggle input:checked + label").css("background", "hsl(6, 63%, 50%)");
+        $(".result-screen").css("color", "hsl(30, 25%, 89%)");
+        $(".result-screen").css("background-color","hsl(224, 36%, 15%)");
+        $(".container-keys").css("background-color","hsl(223, 31%, 20%)");
+        $(".numbersBtn").css("box-shadow", "1px 2px hsl(28, 16%, 65%)");
+        $(".small-button").css("background-color", "hsl(30, 25%, 89%)");
+        $(".small-button").css("color", "hsl(60, 10%, 19%)");
+        $(".delBtn").css("background-color", "hsl(225, 21%, 49%)");
+        $(".delBtn").css("color", "#fff");
+        $(".delBtn").css("box-shadow", "1px 2px hsl(224, 28%, 35%)");
+        $(".big-button").css("background-color", "hsl(225, 21%, 49%)");
+        $(".big-button").css("color", "hsl(0, 0, 100%)");
+        $(".big-button").css("box-shadow", "1px 2px hsl(224, 28%, 35%)");
+        $(".eqlButton").css("background-color", "hsl(6, 63%, 50%)");
+        $(".eqlbutton").css("color", "hsl(0, 0, 100%)");
+        $(".eqlButton").css("box-shadow", "1px 2px hsl(6, 70%, 34%)");
+        $(".attribution").css("color", "hsl(28, 16%, 65%)");
+    }
+});
+
+$("#theme2").click(function() {
+    if($("#theme2").is(":checked")){
+        $("body").css("background-color", "hsl(0, 0%, 90%)");
+        $("a").css("color", "hsl(60, 10%, 19%)");
+        $(".header").css("color", "hsl(60, 10%, 19%)");
+        $(".calcText, .theme-nums, .theme-text").css("color", "hsl(60, 10%, 19%)");
+        $(".switch-toggle").css("background", "hsl(0, 5%, 81%)");
+        $(".switch-toggle label").css("background", "transparent");
+        $(".switch-toggle input:checked + label").css("background", "hsl(6, 63%, 50%)");
+        $(".result-screen").css("color", "hsl(60, 10%, 19%)");
+        $(".result-screen").css("background-color","hsl(0, 0%, 93%)");
+        $(".container-keys").css("background-color","hsl(0, 5%, 81%)");
+        $(".numbersBtn").css("box-shadow", "1px 2px hsl(35, 11%, 61%)");
+        $(".small-button").css("background-color", "hsl(45, 7%, 89%)");
+        $(".small-button").css("color", "hsl(60, 10%, 19%)");
+        $(".delBtn").css("background-color", "hsl(185, 42%, 37%)");
+        $(".delBtn").css("color", "#fff");
+        $(".delBtn").css("box-shadow", "1px 2px hsl(185, 58%, 25%)");
+        $(".big-button").css("background-color", "hsl(185, 42%, 37%)");
+        $(".big-button").css("color", "hsl(0, 0, 100%)");
+        $(".big-button").css("box-shadow", "1px 2px hsl(185, 58%, 25%)");
+        $(".eqlButton").css("background-color", "hsl(25, 98%, 40%)");
+        $(".eqlbutton").css("color", "#fff");
+        $(".eqlButton").css("box-shadow", "1px 2px hsl(25, 99%, 27%)");
+        $(".attribution").css("color", "hsl(35, 11%, 61%)");
+    }
+});
+
+
+
+
+$("#theme3").click(function() {
+    if($("#theme3").is(":checked")){
+        $("body").css("background-color", "hsl(268, 75%, 9%)");
+        $("a").css("color", "hsl(52, 100%, 62%)");
+        $(".header").css("color", "hsl(52, 100%, 62%)");
+        $(".calcText, .theme-nums, .theme-text").css("color", "hsl(52, 100%, 62%)");
+        $(".switch-toggle").css("background", "hsl(268, 71%, 12%)");
+        $(".switch-toggle label").css("background", "transparent");
+        $(".switch-toggle input:checked + label").css("background", "hsl(176, 100%, 44%)");
+        $(".result-screen").css("color", "hsl(52, 100%, 62%)");
+        $(".result-screen").css("background-color","hsl(268, 71%, 12%)");
+        $(".container-keys").css("background-color","hsl(268, 71%, 12%)");
+        $(".numbersBtn").css("box-shadow", "1px 2px hsl(290, 70%, 36%)");
+        $(".small-button").css("background-color", "hsl(268, 47%, 21%)");
+        $(".small-button").css("color", "hsl(52, 100%, 62%)");
+        $(".delBtn").css("background-color", "hsl(281, 89%, 26%)");
+        $(".delBtn").css("color", "#fff");
+        $(".delBtn").css("box-shadow", "1px 2px hsl(285, 91%, 52%)");
+        $(".big-button").css("background-color", "hsl(281, 89%, 26%)");
+        $(".big-button").css("color", "#fff");
+        $(".big-button").css("box-shadow", "1px 2px hsl(285, 91%, 52%)");
+        $(".eqlButton").css("background-color", "hsl(176, 100%, 44%)");
+        $(".eqlbutton").css("color", "hsl(198, 20%, 13%)");
+        $(".eqlButton").css("box-shadow", "1px 2px hsl(177, 92%, 70%)");
+        $(".attribution").css("color", "#fff");
+    }
+});
